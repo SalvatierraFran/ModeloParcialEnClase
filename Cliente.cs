807f58e5cc35abb4f5dc4fe9f892225039902114
SalvatierraFran <franco14_93@hotmail.com>
2016-04-20 18:36:59 -0300

    Modelo de parcial con Cuentas OffShore

---
+++ Cliente.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModeloParcial
+{
+    class Cliente
+    {
+        /* Crear alias
+         * Crear el alias concatenando un numero random de cuatro cifras con la nacionalidad y el
+         * tipo de cliente, invocar a este metodo en el constructor por defecto.
+         * getAleas: retorna el aleas
+         * Retornar datos (de instancia y privado) retorna un string con todos los datos.
+         * Retornar datos (estatico y publico) retorna un string con todos los datos, utilizar el metodo de instancia*/
+
+        private string _aliasParaIncognito;
+        private string _nacionalidad;
+        private string _nombre;
+        private enumeradores.eTipoCliente _tipoDeCliente;
+
+        Random rnd = new Random();
+
+        private Cliente()
+        {
+            CrearAlias();
+        }
+
+        public Cliente(enumeradores.eTipoCliente cliente, string nombre)
+        {
+            this._tipoDeCliente = cliente;
+            this._nombre = nombre;
+        }
+
+        public Cliente(enumeradores.eTipoCliente cliente, string nombre, string nacionalidad)
+        {
+            this._tipoDeCliente = cliente;
+            this._nombre = nombre;
+            this._nacionalidad = nacionalidad;
+        }
+
+        public Cliente(string nombre)
+        {
+            this._nombre = nombre;
+        }
+
+        private void CrearAlias()
+        {
+            int resultado;
+
+            resultado = rnd.Next(1111, 9999);
+
+            string _aliasParaIncognito = this.rnd + this._nacionalidad + this._tipoDeCliente;
+
+        }
+
+        public string getAlias()
+        {
+            if (this._aliasParaIncognito == null)
+            {
+                this.CrearAlias();
+            }
+
+            return this._aliasParaIncognito;
+        }
+
+        private string RetornarDatos()
+        {
+            string resultado;
+
+            resultado = string.Concat(getAlias(), " - ", this._nacionalidad, " - ", this._nombre, " - ", this._tipoDeCliente);
+
+            return resultado;
+        }
+
+        public string RetornarDatos(Cliente UnCliente)
+        {
+            return UnCliente.RetornarDatos();
+        }
+    }
+}
---
+++ CuentaOffShore.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModeloParcial
+{
+    class CuentaOffShore
+    {
+        /*En esta clase el constructor por defecto debera sumar 1 al atributo estatico
+        cantidad de cuenta y en lugar por defecto sera Bahamas, no repetir lineas de codigo.
+        Constructor de clase en esta clase sera el encargado de inicializar la cantidad de cuentas
+        a cero*/
+
+        private enumeradores.eTipoBeneficio beneficio;
+        public static int CantidadDeCuentas;
+        private enumeradores.eParaisosFiscales lugar;
+        private List<Cliente> _ListadoClientes;
+
+        public CuentaOffShore()
+        {
+            CantidadDeCuentas = CantidadDeCuentas + 1;
+            this.lugar = enumeradores.eParaisosFiscales.Bahamas;
+        }
+
+        static CuentaOffShore()
+        {
+            CantidadDeCuentas = 0;
+        }
+
+        public CuentaOffShore(enumeradores.eParaisosFiscales Lugar)
+        {
+            this.lugar = Lugar;
+        }
+
+        public CuentaOffShore(enumeradores.eParaisosFiscales Lugar, enumeradores.eTipoBeneficio Beneficio)
+        {
+            this.lugar = Lugar;
+            this.beneficio = Beneficio;
+        }
+
+        public void FiltrarTodosLosDatos()
+        { }
+
+        public int getCantidadDeClientes()
+        {
+            int cantidad;
+
+            cantidad = this._ListadoClientes.Count();
+
+            return cantidad;
+        }
+
+        private void MostrarClientes()
+        {
+            foreach (Cliente item in this._ListadoClientes)
+            {
+                Console.WriteLine(item.RetornarDatos());
+            }
+        }
+
+        public static bool operator !=(CuentaOffShore cuenta, Cliente cliente)
+        { }
+
+        public static CuentaOffShore operator +(CuentaOffShore cuenta, Cliente cliente)
+        { }
+
+        public static bool operator ==(CuentaOffShore cuenta, Cliente cliente)
+        { }
+    }
+}
+
+namespace enumeradores
+{
+    public enum eParaisosFiscales
+    {
+        Anguila, Bahamas, Belice, Delaware, Gibraltar, Hong_Kong, Mauricio, Panamá, Seychelles, Singapur, Suiza
+    }
+
+    public enum eTipoProducto
+    {
+        cuentas_bancarias, pólizas_de_seguros, inversiones_inmoviliarias, sociedades_extranjeras, fondos_de_inversion
+    }
+
+    public enum eTipoBeneficio
+    {
+        secreto_bancario, beneficios_fiscales, secreto_bancario_y_beneficios_fiscales
+    }
+
+    public enum eTipoCliente
+    {
+        PolíticoCorrupto, EmpresarioCorrupto, jugadorDeFutbol, financista, maestroRural
+    }
+}
